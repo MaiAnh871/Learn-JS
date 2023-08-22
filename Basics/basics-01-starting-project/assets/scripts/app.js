5bdcaadda589fc25fa501f4e53383886bbbbec5e
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-22 15:41:18 +0700

    CHECPOINT: basics 07 - function with return

--- Basics/basics-01-starting-project/assets/scripts/app.js
+++ Basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,20 +1,16 @@
 const defaultResult = 0;
-
 let currentResult = defaultResult;
 
-// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 let errorMessage = "An error \n" + "occurred!";
 
 function add(num1, num2) {
     const result = num1 + num2;
-    alert("The result was " + result);
+    // alert("The result was " + result);
+    return result;
 }
 
-add(1, 2);
-add(2, 3);
+currentResult = add(1, 2);
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
