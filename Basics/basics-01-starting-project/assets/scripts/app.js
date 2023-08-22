3eba991b8745f457186e2e77f0cdee9bcf385e10
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-22 14:54:18 +0700

    CHECKPOINT: basics 05 - added template literals

--- Basics/basics-01-starting-project/assets/scripts/app.js
+++ Basics/basics-01-starting-project/assets/scripts/app.js
@@ -2,8 +2,12 @@ const defaultResult = 0;
 
 let currentResult = defaultResult;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+// let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+let errorMessage =  'An error \n' + 
+                    'occurred!';
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, calculationDescription);
+outputResult(currentResult, errorMessage);
