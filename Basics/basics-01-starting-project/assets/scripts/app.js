d291b5a6cce21ebc5a8b340f53f0e49307b8c877
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-22 14:18:30 +0700

    CHECKPOINT: basics 04 - added constant

--- Basics/basics-01-starting-project/assets/scripts/app.js
+++ Basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,5 +1,9 @@
-let currentResult = 0;
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescription);
