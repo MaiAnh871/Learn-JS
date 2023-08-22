d39c487f30b61343f1e61530503657d2e5e4a0a0
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-22 17:35:56 +0700

    CHECKPOINT: basics 10 - function refactoring

--- Basics/basics-01-starting-project/assets/scripts/app.js
+++ Basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,15 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add() {
-    currentResult = currentResult + parseInt(userInput.value);
-    // currentResult = currentResult + +userInput.value;
-    // currentResult = currentResult.toString() + parseInt(userInput.value);
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
 
-    outputResult(currentResult, "");
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
 }
 
 addBtn.addEventListener("click", add);
