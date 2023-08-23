0b017f5c7ae1943089ce1a91d7260d5b3322d211
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-23 09:39:38 +0700

    CHECKPOINT: basics 11 - subtract multiply divide

--- Basics/basics-01-starting-project/assets/scripts/app.js
+++ Basics/basics-01-starting-project/assets/scripts/app.js
@@ -5,11 +5,40 @@ function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
+function createAndWriteLog(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(resultBeforeCalc, calcDescription);
+}
+
 function add() {
     const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
+    createAndWriteLog("+", initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog("-", initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog("*", initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog("/", initialResult, enteredNumber);
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
