78bee82a77fb27008286db83d34ebcd39bd4466f
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-29 14:39:35 +0700

    CHECKPOINT: behind scene 02 - block scope

---
+++ Behind the Scenes/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,16 @@
+var name = "Max";
+
+if (name === "Max") {
+    let hobbies = ["Sports", "Cooking"];
+    console.log(hobbies);
+}
+
+function greet() {
+    var age = 30;
+    var name = "Manuel";
+    console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies);
+
+greet();
