28b6658e6797dd1be9b81abe9d0775aecc5ac66e
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-08-31 15:29:32 +0700

    CHECKPOINT: function 02 - function expression

---
+++ More on Functions/functions-01-starting-code/app.js
@@ -0,0 +1,16 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const start = function() {
+    console.log("Game is starting");
+};
+
+// const person = {
+//     name: "Max",
+//     greet: function greet() {
+//         console.log("Hello there!");
+//     },
+// };
+
+// person.greet();
+
+startGameBtn.addEventListener("click", start);
