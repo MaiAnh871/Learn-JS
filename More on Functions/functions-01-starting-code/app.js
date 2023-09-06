ec726de485f66310db7a847601b507f8276f0f49
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-06 10:07:43 +0700

    CHECKPOINT: function 08 - rest parameter

--- More on Functions/functions-01-starting-code/app.js
+++ More on Functions/functions-01-starting-code/app.js
@@ -72,3 +72,23 @@ startGameBtn.addEventListener("click", () => {
     alert(message);
     gameIsRunning = false;
 });
+
+// Not related to the game above
+const sumUp = (a, b, ...numbers) => {
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num;
+    }
+    return sum;
+};
+
+const subtractUp = function (...numbers) {
+    let sum = 0;
+    for (const num of arguments) {
+        sum -= num;
+    }
+    return sum;
+};
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(subtractUp(1, 5, 10, -3, 6, 10));
