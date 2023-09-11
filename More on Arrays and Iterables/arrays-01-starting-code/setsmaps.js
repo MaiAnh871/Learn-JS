27ee62f5e5247822338ae52f68e6bd3bd0c1c8d1
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-11 09:53:12 +0700

    CHECKPOINT: array 18 - maps

--- More on Arrays and Iterables/arrays-01-starting-code/setsmaps.js
+++ More on Arrays and Iterables/arrays-01-starting-code/setsmaps.js
@@ -1,10 +1,33 @@
-// const ids = new Set([1, 2, 3]);
-const ids = new Set(["Hi", "from", "Set"]);
-ids.add(2);
-if (ids.has(2)) {
-    ids.delete("Hi");
+// // const ids = new Set([1, 2, 3]);
+// const ids = new Set(["Hi", "from", "Set"]);
+// ids.add(2);
+// if (ids.has(2)) {
+//     ids.delete("Hi");
+// }
+
+// for (const entry of ids.entries()) {
+//     console.log(entry[0]);
+// }
+
+const person1 = { name: "Max" };
+const person2 = { name: "Manuel" };
+
+const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+
+personData.set(person2, [{ date: "two weeks ago", price: 100 }]);
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const entry of personData.entries()) {
+    console.log(entry);
 }
 
-for (const entry of ids.entries()) {
-    console.log(entry[0]);
+for (const [key, value] of personData.entries()) {
+    console.log(key, value);
 }
+
+for (const key of personData.keys()) {
+    console.log(key);
+}
+
+console.log(personData.size);
