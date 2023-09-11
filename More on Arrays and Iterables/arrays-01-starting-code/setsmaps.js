8a00cb3a1c27df03847240295ffa61a5f1cbd000
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-11 10:22:52 +0700

    CHECKPOINT: array 19 - weak sets weak maps

--- More on Arrays and Iterables/arrays-01-starting-code/setsmaps.js
+++ More on Arrays and Iterables/arrays-01-starting-code/setsmaps.js
@@ -9,25 +9,49 @@
 //     console.log(entry[0]);
 // }
 
-const person1 = { name: "Max" };
-const person2 = { name: "Manuel" };
+// const person1 = { name: "Max" };
+// const person2 = { name: "Manuel" };
 
-const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+// const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
 
-personData.set(person2, [{ date: "two weeks ago", price: 100 }]);
-console.log(personData);
-console.log(personData.get(person1));
+// personData.set(person2, [{ date: "two weeks ago", price: 100 }]);
+// console.log(personData);
+// console.log(personData.get(person1));
 
-for (const entry of personData.entries()) {
-    console.log(entry);
-}
+// for (const entry of personData.entries()) {
+//     console.log(entry);
+// }
 
-for (const [key, value] of personData.entries()) {
-    console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//     console.log(key, value);
+// }
 
-for (const key of personData.keys()) {
-    console.log(key);
-}
+// for (const key of personData.keys()) {
+//     console.log(key);
+// }
 
-console.log(personData.size);
+// console.log(personData.size);
+
+let person = { name: "Max" };
+
+const persons = new WeakSet();
+persons.add(person);
+console.log(persons);
+
+const persons2 = new Set();
+persons2.add(person);
+console.log(persons2);
+
+const persons3 = new WeakMap();
+persons3.set(person, "Extra info!");
+console.log(persons3);
+
+const persons4 = new Map();
+persons4.set(person, "Extra info!");
+console.log(persons4);
+
+person = null;
+console.log(persons);
+console.log(persons2);
+console.log(persons3);
+console.log(persons4);
