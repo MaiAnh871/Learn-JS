0c7940305fd1be465323894cecf40ac72574bfa6
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-25 17:16:17 +0700

    CHECKPOINT: objects 15 - getter setter

--- More on Objects/objects-11-introducing-this/assets/scripts/objects.js
+++ More on Objects/objects-11-introducing-this/assets/scripts/objects.js
@@ -1,76 +1,88 @@
-const addMovieBtn = document.getElementById('add-movie-btn');
-const searchBtn = document.getElementById('search-btn');
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = (filter = '') => {
-  const movieList = document.getElementById('movie-list');
+const renderMovies = (filter = "") => {
+    const movieList = document.getElementById("movie-list");
 
-  if (movies.length === 0) {
-    movieList.classList.remove('visible');
-    return;
-  } else {
-    movieList.classList.add('visible');
-  }
-  movieList.innerHTML = '';
-
-  const filteredMovies = !filter
-    ? movies
-    : movies.filter(movie => movie.info.title.includes(filter));
-
-  filteredMovies.forEach(movie => {
-    const movieEl = document.createElement('li');
-    const { info, ...otherProps } = movie;
-    console.log(otherProps);
-    // const { title: movieTitle } = info;
-    // const { getFormattedTitle } = movie;
-    let text = movie.getFormattedTitle() + ' - ';
-    for (const key in info) {
-      if (key !== 'title') {
-        text = text + `${key}: ${info[key]}`;
-      }
+    if (movies.length === 0) {
+        movieList.classList.remove("visible");
+        return;
+    } else {
+        movieList.classList.add("visible");
     }
-    movieEl.textContent = text;
-    movieList.append(movieEl);
-  });
+    movieList.innerHTML = "";
+
+    const filteredMovies = !filter
+        ? movies
+        : movies.filter((movie) => movie.info.title.includes(filter));
+
+    filteredMovies.forEach((movie) => {
+        const movieEl = document.createElement("li");
+        const { info, ...otherProps } = movie;
+        console.log(otherProps);
+        // const { title: movieTitle } = info;
+        // const { getFormattedTitle } = movie;
+        let text = movie.getFormattedTitle() + " - ";
+        for (const key in info) {
+            if (key !== "title" && key !== "_title") {
+                text = text + `${key}: ${info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    });
 };
 
 const addMovieHandler = () => {
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+    const title = document.getElementById("title").value;
+    const extraName = document.getElementById("extra-name").value;
+    const extraValue = document.getElementById("extra-value").value;
 
-  if (
-    title.trim() === '' ||
-    extraName.trim() === '' ||
-    extraValue.trim() === ''
-  ) {
-    return;
-  }
+    if (
+        title.trim() === "" ||
+        extraName.trim() === "" ||
+        extraValue.trim() === ""
+    ) {
+        return;
+    }
 
-  const newMovie = {
-    info: {
-      title,
-      [extraName]: extraValue
-    },
-    id: Math.random().toString(),
-    getFormattedTitle: function() {
-      /* 
+    const newMovie = {
+        info: {
+            set title(val) {
+                if (val.trim() === "") {
+                    this._title = "DEFAULT";
+                    return;
+                }
+                this._title = val;
+            },
+            get title() {
+                return this._title.toUpperCase();
+            },
+            [extraName]: extraValue,
+        },
+        id: Math.random().toString(),
+        getFormattedTitle: function () {
+            /* 
       Inside one function, nomatter that function is inside one object or not, this
       keyword will reference to what called that function.
       */
-      return this.info.title.toUpperCase();
-    }
-  };
+            return this.info.title.toUpperCase();
+        },
+    };
 
-  movies.push(newMovie);
-  renderMovies();
+    newMovie.info.title = title;
+    console.log(newMovie.info.title);
+
+    movies.push(newMovie);
+    renderMovies();
 };
 
 const searchMovieHandler = () => {
-  const filterTerm = document.getElementById('filter-title').value;
-  renderMovies(filterTerm);
+    const filterTerm = document.getElementById("filter-title").value;
+    renderMovies(filterTerm);
 };
 
-addMovieBtn.addEventListener('click', addMovieHandler);
-searchBtn.addEventListener('click', searchMovieHandler);
+addMovieBtn.addEventListener("click", addMovieHandler);
+searchBtn.addEventListener("click", searchMovieHandler);
