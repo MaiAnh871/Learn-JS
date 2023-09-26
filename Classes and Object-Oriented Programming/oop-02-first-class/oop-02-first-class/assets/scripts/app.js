97c384b982aa064e4bfaad2cef47b533c092c4e9
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-26 16:30:54 +0700

    CHECKPOINT: oop 10 - render in constructor

--- Classes and Object-Oriented Programming/oop-02-first-class/oop-02-first-class/assets/scripts/app.js
+++ Classes and Object-Oriented Programming/oop-02-first-class/oop-02-first-class/assets/scripts/app.js
@@ -18,7 +18,10 @@ class Component {
     constructor(renderHookId) {
         this.hookId = renderHookId;
         console.log("Component called constructor!");
+        this.render();
     }
+
+    render() {}
 
     createRootElement(tag, cssClasses, attributes) {
         const rootElement = document.createElement(tag);
@@ -126,18 +129,19 @@ class ProductList extends Component {
             new ElementAttribute("id", "prod-list"),
         ]);
         for (const prod of this.products) {
-            const productItem = new ProductItem(prod, "prod-list");
-            productItem.render();
+            new ProductItem(prod, "prod-list");
         }
     }
 }
 
 class Shop {
+    constructor() {
+        this.render();
+    }
+
     render() {
         this.cart = new ShoppingCart("app");
-        this.cart.render();
-        const productList = new ProductList("app");
-        productList.render();
+        new ProductList("app");
     }
 }
 
@@ -146,7 +150,6 @@ class App {
 
     static init() {
         const shop = new Shop();
-        shop.render();
         this.cart = shop.cart;
     }
 
