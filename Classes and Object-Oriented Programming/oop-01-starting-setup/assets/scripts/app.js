d2ab1cd17a773e69c06cecb109b5ebfe1c41e855
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-09-26 10:26:34 +0700

    CHECKPOINT: oop 03 - productlist productitem classes

--- Classes and Object-Oriented Programming/oop-01-starting-setup/assets/scripts/app.js
+++ Classes and Object-Oriented Programming/oop-01-starting-setup/assets/scripts/app.js
@@ -3,10 +3,50 @@ class Product {
     imageUrl;
     description;
     price;
+
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
 }
 
-console.log(new Product());
-
 const productList = {
-    products: [new Product()],
+    products: [
+        new Product(
+            "A Pollow",
+            "https://m.media-amazon.com/images/I/61XxtPdqhZL.jpg",
+            "A soft pillow",
+            29.99
+        ),
+        new Product(
+            "A Carpet",
+            "https://contentgrid.homedepot-static.com/hdus/en_US/DTCCOMNEW/Articles/types-of-rugs-2022-section-4.jpg",
+            "A soft carpet",
+            59.99
+        ),
+    ],
+    render() {
+        const renderHook = document.getElementById("app");
+        const prodList = document.createElement("ul");
+        prodList.className = "product-list";
+        for (const prod of this.products) {
+            const prodEl = document.createElement("li");
+            prodEl.className = "product-item";
+            prodEl.innerHTML = `
+            <div>
+              <img src="${prod.imageUrl}" alt="${prod.title}" >
+              <div class="product-item__content">
+                <h2>${prod.title}</h2>
+                <h3>\$${prod.price}</h3>
+                <p>${prod.description}</p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          `;
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+    },
 };
