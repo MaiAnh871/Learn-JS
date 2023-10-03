1cc8111034f777d0a08ae333206d75f9a014ee4b
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-10-03 16:59:35 +0700

    CHECKPOINT: http 03 - sending post request

--- Working with HTTP Requests/http-01-starting-setup/assets/scripts/app.js
+++ Working with HTTP Requests/http-01-starting-setup/assets/scripts/app.js
@@ -1,15 +1,30 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url, data) {
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+        xhr.open(method, url);
 
-xhr.responseType = "json";
+        xhr.responseType = "json";
 
-xhr.onload = function () {
-    //const listOfPosts = JSON.parse(xhr.response);
-    const listOfPosts = xhr.response;
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+
+        xhr.send(JSON.stringify(data));
+    });
+
+    return promise;
+}
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest(
+        "GET",
+        "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPosts = responseData;
     console.log(listOfPosts);
     for (const post of listOfPosts) {
         const postEl = document.importNode(postTemplate.content, true);
@@ -17,6 +32,18 @@ xhr.onload = function () {
         postEl.querySelector("p").textContent = post.body;
         listElement.append(postEl);
     }
-};
+}
 
-xhr.send();
+async function createPost(title, content) {
+    const userId = Math.random();
+    const post = {
+        title: title,
+        body: content,
+        userId: userId,
+    };
+
+    sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchPosts();
+createPost("DUMMY", "A dummy post");
