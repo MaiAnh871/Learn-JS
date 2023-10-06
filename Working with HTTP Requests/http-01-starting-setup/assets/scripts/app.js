e0102f8eef643d7dc364f10606611d6a37a1a775
MaiAnh871 <anh.ttm193288@sis.hust.edu.vn>
2023-10-06 09:08:24 +0700

    CHECKPOINT: http 08 -  post request with fetch

--- Working with HTTP Requests/http-01-starting-setup/assets/scripts/app.js
+++ Working with HTTP Requests/http-01-starting-setup/assets/scripts/app.js
@@ -5,49 +5,56 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-    const promise = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+    // const promise = new Promise((resolve, reject) => {
+    //     const xhr = new XMLHttpRequest();
 
-        xhr.open(method, url);
+    //     xhr.open(method, url);
 
-        xhr.responseType = "json";
+    //     xhr.responseType = "json";
 
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject(new Error("Something went wrong!"));
-            }
-        };
+    //     xhr.onload = function () {
+    //         if (xhr.status >= 200 && xhr.status < 300) {
+    //             resolve(xhr.response);
+    //         } else {
+    //             reject(new Error("Something went wrong!"));
+    //         }
+    //     };
 
-        xhr.onerror = function () {
-            reject(new Error("Failed to send request"));
-        };
+    //     xhr.onerror = function () {
+    //         reject(new Error("Failed to send request"));
+    //     };
 
-        xhr.send(JSON.stringify(data));
+    //     xhr.send(JSON.stringify(data));
+    // });
+
+    // return promise;
+
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+    }).then((response) => {
+        return response.json();
     });
-
-    return promise;
 }
 
 async function fetchPosts() {
-    try {
-        const responseData = await sendHttpRequest(
-            "GET",
-            "https://jsonplaceholder.typicode.com/poss"
-        );
-        const listOfPosts = responseData;
-        console.log(listOfPosts);
-        for (const post of listOfPosts) {
-            const postEl = document.importNode(postTemplate.content, true);
-            postEl.querySelector("h2").textContent = post.title.toUpperCase();
-            postEl.querySelector("p").textContent = post.body;
-            postEl.querySelector("li").id = post.id;
-            listElement.append(postEl);
-        }
-    } catch (error) {
-        console.log(error);
+    // try {
+    const responseData = await sendHttpRequest(
+        "GET",
+        "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPosts = responseData;
+    console.log(listOfPosts);
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector("h2").textContent = post.title.toUpperCase();
+        postEl.querySelector("p").textContent = post.body;
+        postEl.querySelector("li").id = post.id;
+        listElement.append(postEl);
     }
+    // } catch (error) {
+    //     console.log(error);
+    // }
 }
 
 async function createPost(title, content) {
